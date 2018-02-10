3ae9a97700ceca70e606fa12ca1478959eb8dd58
gusplayer <fosterstudios.colombia@gmail.com>
2018-02-10 12:41:47 -0500

    Add card with hotels

--- App.js
+++ App.js
@@ -1,23 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Card, ListItem, Button, FlatList } from 'react-native-elements'
+
+const users = [
+  {
+     name: 'brynn',
+     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+  }
+]
 
 export default class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotels: [
+        {
+          "name": "Gaspar Brasserie",
+          "stars": "185 Sutter St, San Francisco, CA 94109",
+          "images": [
+            { "url": "https://shoutem.github.io/static/getting-started/restaurant-1.jpg" },
+            { "url": "https://shoutem.github.io/static/getting-started/restaurant-1.jpg" },
+            { "url": "https://shoutem.github.io/static/getting-started/restaurant-1.jpg" },
+          ],
+          "price" : "50000"
+        }, 
+        {
+          "name": "Gaspar Brasserie",
+          "stars": "185 Sutter St, San Francisco, CA 94109",
+          "images": [
+            { "url": "https://shoutem.github.io/static/getting-started/restaurant-1.jpg" },
+            { "url": "https://shoutem.github.io/static/getting-started/restaurant-1.jpg" },
+            { "url": "https://shoutem.github.io/static/getting-started/restaurant-1.jpg" },
+          ],
+          "price" : "50000"
+        },        
+      ],
+    }
+  }
+
+  render(){
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+        <Text>hola aqui</Text>
+
+        <Card title="CARD WITH DIVIDER">
+        {
+          this.state.hotels.map((u, i) => {
+            return (
+              <View key={i} style={styles.user}>
+                <Image
+                  style={styles.image}
+                  resizeMode="cover"
+                  source={{ uri: u.images[0].url }}
+                />
+                <Text style={styles.name}>{u.name}</Text>
+              </View>
+            );
+          })
+        }
+      </Card>
+    
+      </View>      
+    )
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: 400,
+    height: 400
+  },
+  name:{
+
+  },
+  user:{
+
+  }
 });
